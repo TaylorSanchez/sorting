9b270743b983cdd7c8a93db50718d92656b1f627
Taylor Sanchez <xTaylorSanchez@gmail.com>
2014-01-25 20:32:10 -0800

    More Cleanup on moveNode
    add -1 as an option to move last, or move to last. 0 to move first and
    move to first

--- merge.cpp
+++ merge.cpp
@@ -15,72 +15,195 @@ linkedList* createLinkedList(){
 	return head;
 }
 
-linkedList* addToBeginning(int newData, linkedList *head){
+linkedList* insertToBeginning(int newData, linkedList *head){
 	//temporary pointer for 'juggling' nodes, also first data holder
-	linkedList *temp = NULL;
-	temp = (linkedList*)malloc(sizeof(linkedList));
+	linkedList *newHead = NULL;
+	newHead = (linkedList*)malloc(sizeof(linkedList));
 
-	temp->data = newData;
-	temp->next = head;
-	return head = temp;
+	newHead->data = newData;
+	newHead->next = head;
+	return head = newHead;
 }
 
-void addToEnd(int newData, linkedList *head){
+void insertToEnd(int newData, linkedList *head){
 	//This adds a node to the END of the linkedList
 	linkedList *temp = NULL;
 	linkedList *end = NULL;
 	end = (linkedList*)malloc(sizeof(linkedList));
-	temp = (linkedList*)malloc(sizeof(linkedList));
 
-	temp = head;              // transfer the address of 'head' to 'end'
-	while(temp->next!=NULL){ // go to the last node
-		temp = temp->next;//tranfer the address of 'end1->next' to 'end'
+	temp = head;
+	while(temp->next!=NULL){
+		temp = temp->next;
   	}
-	end->data = newData;                   // store data(first field)
-	end->next = NULL;                  // second field will be null(last node)
+	end->data = newData;
+	end->next = NULL;
 	temp->next = end;
 }
 
-void addtoMid(int newData, int nodeLocation, linkedList *head){
+void insertToMid(int newData, int nodeLocation, linkedList *head){
 	//inserting after 'x' number of nodes
 	linkedList *temp = NULL;
 	linkedList *mid = NULL;
 	mid = (linkedList*)malloc(sizeof(linkedList));
-	temp = (linkedList*)malloc(sizeof(linkedList));
-
 	temp = head;
 	for( int i = 1; i <= nodeLocation ; i++ ){
-		if( temp == NULL ){
-	    	printf("Node %i does not exist.\n", nodeLocation);
+		if( temp->next == NULL ){
+	    	printf("Node %i does not exist,"
+	    	       " added to end Node.\n", nodeLocation);
+	    	mid->data = newData;
+	    	mid->next = NULL;
+	    	temp->next = mid;
 	    	break;
 	    }
 
 	    else if ( i == nodeLocation ){
-	      	mid = (linkedList*)malloc(sizeof(linkedList));
 	      	mid->data = newData;
 	      	mid->next = temp->next;
 	      	temp->next = mid;
 	      	break;
 	    }
-	   temp = temp->next;           // go to the next node
+	   temp = temp->next;
 	}
+}
+
+
+linkedList* insertNode(int newData, int nodeLocation, linkedList *head){
+	if ( nodeLocation == 0 ){
+		head = insertToBeginning(newData, head);
+	}
+	else if ( nodeLocation == -1 )
+	{
+		insertToEnd(newData, head);
+	}
+	else{
+		insertToMid(newData, nodeLocation, head);
+	}
+	return head;
 }
 
 void outputLinkedList(linkedList* head){
 	//This will output each node in the linkedList
 	linkedList *end;
-	end=(linkedList*)malloc(sizeof(linkedList));
   	end=head;
   	int i = 0;
 	while( end!=NULL ){
-		printf("\n");
-		cout<< end->data<<" ";// show the data in the linked list
-		cout<< end->next<<" ";
+		printf("%i ",end->data );
+		printf("%p\n",end->next );
 		end = end->next;   // tranfer the address of 'end->next' to 'end'
 		if (i > 9){break;}
 		i++;
 	}
 	printf("\n");
+}
+
+void moveToLast(linkedList *tempPtr, linkedList *stepPtr, int newLocation){
+	printf("When looking for newLocation, "
+		"Node %i, reached end of linkedList, "
+		"moved to last Node.\n", newLocation);
+	tempPtr->next = NULL;
+	stepPtr->next = tempPtr;
+}
+
+linkedList* moveFirstNode(int newLocation, linkedList* tempPtr,
+                          linkedList* stepPtr, linkedList* head)
+{
+	tempPtr = stepPtr;
+	head = stepPtr->next;
+	//update pionter to point past the one we are moving
+	stepPtr->next = tempPtr->next;
+	//start back at the top:
+	stepPtr = head;
+	for( int j=0; j <=  newLocation; j++){
+		if( stepPtr->next == NULL ){
+			moveToLast(tempPtr, stepPtr, newLocation);
+			break;
+		}
+		else if ( j == newLocation - 2 ){
+			tempPtr->next = stepPtr->next;
+			stepPtr->next = tempPtr;
+			break;
+		}
+		stepPtr = stepPtr->next;
+	}
+	return head;
+}
+
+linkedList* moveMidNode(int currentLocation, int newLocation,
+                        linkedList* tempPtr,linkedList* stepPtr,
+                        linkedList* head )
+{
+	for( int i = 0; i <= currentLocation ; i++ ){
+	    if( stepPtr->next == NULL ){
+	    	printf("Node %i for currentLocation does not exist. "
+	    	       "Exiting moveNode early.\n", currentLocation );
+	    	break;
+	    }
+	    else if ( i == currentLocation-2 ){
+	    	tempPtr = stepPtr->next;
+	    	stepPtr->next = tempPtr->next;
+	    	stepPtr = head;
+	    	if ( newLocation == -1 ){
+	    		while( stepPtr->next != NULL ){ stepPtr = stepPtr->next; }
+			    moveToLast( tempPtr, stepPtr, newLocation );
+			    break;
+	    	}
+	    	for( int j=0; j <=  newLocation; j++){
+	    		if ( newLocation == 1 || newLocation == 0){
+	    			tempPtr->next = head;
+	    			head = tempPtr;
+	    			break;
+	    		}
+				else if( stepPtr->next == NULL ){
+			    	moveToLast( tempPtr, stepPtr, newLocation );
+	    			break;
+	    		}
+	    		else if ( j == newLocation - 2 ){
+	    			tempPtr->next = stepPtr->next;
+	    			stepPtr->next = tempPtr;
+	    			break;
+	    		}
+	    		stepPtr = stepPtr->next;
+	    	}
+	    	break;
+	    }
+		stepPtr = stepPtr->next;
+	}
+	return head;
+}
+
+linkedList* moveLastNode(int newLocation, linkedList* tempPtr,
+                         linkedList* stepPtr, linkedList* head)
+{
+//need to rearrange some logic here for moving lastnode
+	while( stepPtr->next != NULL ){ stepPtr = stepPtr->next; }
+	tempPtr = stepPtr->next;
+	stepPtr->next = NULL;
+	stepPtr = head;
+	if ( newLocation == -1 ){
+		while( stepPtr->next != NULL ) { stepPtr = stepPtr->next; }
+		moveToLast( tempPtr, stepPtr, newLocation );
+	}
+	for ( int j=0; j <=  newLocation; j++){
+		printf("%i \n",j ); // debugging
+		if ( newLocation == 1 || newLocation == 0){
+			tempPtr->next = head;
+			head = tempPtr;
+			break;
+		}
+		else if ( stepPtr->next == NULL ){
+	    	moveToLast( tempPtr, stepPtr, newLocation );
+			break;
+		}
+		else if ( j == newLocation - 2 ){
+			printf("asdf\n");
+			tempPtr->next = NULL; //seg fault
+			printf("asdf\n");
+			stepPtr->next = tempPtr;
+			break;
+		}
+		stepPtr = stepPtr->next;
+	}
+	return head;
 }
 
 linkedList* moveNode(int currentLocation,int newLocation,linkedList* head){
@@ -90,90 +213,44 @@ linkedList* moveNode(int currentLocation,int newLocation,linkedList* head){
 	linkedList *tempPtr;
 	stepPtr = head;
 	//look for currentLocation
-	if ( currentLocation == 1 ){
-		tempPtr = stepPtr;
-    	head = stepPtr->next;
-    	//update pionter to point past the one we are moving
-    	stepPtr->next = tempPtr->next;
-    	//start back at the top:
-    	stepPtr = head;
-    	for( int j=0; j <=  newLocation; j++){
-    		if( stepPtr->next == NULL ){
-    			printf("When looking for newLocation, "
-    			       "Node %i, reached end of linkedList, "
-    			       "added to last Node.\n", newLocation);
-    			tempPtr->next = NULL;
-    			stepPtr->next = tempPtr;
-    			break;
-    		}
-    		else if ( j == newLocation - 2 ){
-    			tempPtr->next = stepPtr->next;
-    			stepPtr->next = tempPtr;
-    			break;
-    		}
-    		stepPtr = stepPtr->next;
-    	}
+	if ( currentLocation == -1 )
+	{
+		head = moveLastNode(newLocation, tempPtr, stepPtr, head);
+	}
+	else if ( currentLocation == 0 || currentLocation == 1 ){
+		head = moveFirstNode(newLocation, tempPtr, stepPtr, head);
 	}
 	else {
-		for( int i = 0; i <= currentLocation ; i++ ){
-		    if( stepPtr->next == NULL ){
-		    	printf("Node %i for currentLocation does not exist. "
-		    	       "Exiting moveNode early.\n", currentLocation);
-		    	break;
-		    }
-		    //need to stop at 2 previous for Ptr
-		    else if ( i == currentLocation-2 ){
-		    	tempPtr = stepPtr->next;
-		    	//update pionter to point past the one we are moving
-		    	stepPtr->next = tempPtr->next;
-		    	//start back at the top:
-		    	stepPtr = head;
-		    	for( int j=0; j <=  newLocation; j++){
-		    		if( stepPtr->next == NULL ){
-		    			printf("Node %i for newLocation does not exist. "
-		    			       "Added to last location instead.\n", newLocation);
-		    			tempPtr->next = NULL;
-		    			stepPtr->next = tempPtr;
-		    			break;
-		    		}
-		    		else if ( j == newLocation - 2 ){
-		    			tempPtr->next = stepPtr->next;
-		    			stepPtr->next = tempPtr;
-		    			break;
-		    		}
-		    		stepPtr = stepPtr->next;
-		    	}
-		    	break;
-		    }
-			stepPtr = stepPtr->next;
-		}
+		head = moveMidNode(currentLocation, newLocation, tempPtr, stepPtr,
+		                   head);
 	}
 	return head;
+	// if (newLocation == -1)
+	// {
+	// 	/* code */
+	// }
 }
-
 
 int main(){
 	int node_number;
 	//first pointer to start/header of linked list,
 	//this will be used to add to the beginning of the linkedList
 	linkedList *head = createLinkedList();
-	head = addToBeginning(345, head);
-	head = addToBeginning(346, head);
-	head = addToBeginning(347, head);
-	addToEnd(344, head);
-	addToEnd(343, head);
-	addToEnd(342, head);
-	addToEnd(341, head);
+	head = insertToBeginning(345, head);
+	head = insertToBeginning(346, head);
+	head = insertToBeginning(347, head);
+	insertToEnd(344, head);
+	insertToEnd(343, head);
+	insertToEnd(342, head);
+	insertToEnd(341, head);
 
-	// cout << "ENTER THE NODE LOCATION TO ADD NUMBER:";
-	// cin >> node_number;  // take the node number from user
-	addtoMid(5000, 3, head);
+	insertToMid(5000, 5, head);
 
-
+	head = insertNode(80085,7,head);
 
 	outputLinkedList(head);
 
-	head = moveNode(1,8,head);
+	head = moveNode(-1,3,head); //add -1 to choose last one
 
 	outputLinkedList(head);
 
